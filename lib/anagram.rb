4eb68f21321fe32da0224a2259892949dd090d5c
donvine mugendi <donvinemugendi@gmail.com>
2023-06-29 00:08:20 +0300

    passed tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :listen
+    def initialize(listen)
+        @listen = listen
+    end
+    def match (words)
+        sorted_word = @listen.downcase.chars.sort.join
+        words.select { |w| w.downcase.chars.sort.join == sorted_word } || []
+    end
+end
